8ce076288d7a5e045b885fd4833c727de362c9cf
Andre Gonzales <gibstock@gmail.com>
2022-03-01 16:48:52 -0800

    update UI

--- public/js/script.js
+++ public/js/script.js
@@ -2,9 +2,13 @@ const searchBtn = document.getElementById('searchBtn')
 const artistSearch = document.getElementById('artist-search')
 const searchAgainBtn = document.getElementById('search-again')
 const searchNav = document.getElementById('search-nav')
+const loader = document.getElementById('loader-wrapper')
+const searchBox = document.getElementById('search-box')
 
 function searchArtist() {
   location.assign(`/artist/${artistSearch.value}`)
+  searchBox.style.display = 'none'
+  loader.style.display = 'flex'
 }
 
 function openSearchBox() {
